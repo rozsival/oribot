654e742d16bb874b8880e8e21be7b2aa864b50da
Vít Rozsíval <mail@vitrozsival.cz>
2022-09-08 11:13:57 +0200

    chore: update linter scripts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,5 +3,5 @@ const eslint = 'eslint --cache --fix';
 module.exports = {
   './**/*.js': eslint,
   './**/*.ts': [() => 'tsc --project tsconfig.json', eslint],
-  './**/*.{json,md}': 'prettier --write',
+  './**/*.{html,json,md}': 'prettier --write',
 };
