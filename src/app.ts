e81011392654e2e91237e4a507399d3556f9540f
Vít Rozsíval <mail@vitrozsival.cz>
2022-09-11 12:13:25 +0200

    fix: fix schedule with RecurrenceRule

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
 /* eslint-disable unicorn/no-process-exit */
-import { Range, scheduleJob } from 'node-schedule';
+import { Range, RecurrenceRule, scheduleJob } from 'node-schedule';
 
 import { JOB_HOUR, JOB_MONTH_END, JOB_MONTH_START, TZ } from './environment';
 import { logger } from './logger';
 import { run } from './run';
 
-const job = scheduleJob(
-  { hour: JOB_HOUR, month: new Range(JOB_MONTH_START, JOB_MONTH_END), tz: TZ },
-  run,
-);
+const rule = new RecurrenceRule();
+rule.hour = JOB_HOUR;
+rule.minute = 0;
+rule.month = new Range(JOB_MONTH_START, JOB_MONTH_END);
+rule.tz = TZ;
+
+const job = scheduleJob(rule, run);
+
 logger.logSuccess('Slack bot messaging scheduled');
+logger.logInfo(
+  JSON.stringify({ JOB_MONTH_START, JOB_MONTH_END, JOB_HOUR, TZ }),
+);
 
 const shutdownGracefully = () => {
   job.cancel();
